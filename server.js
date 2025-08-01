ee851c486f5c6e042c48b9372cc4b8b39d5980f6
lakshya369 <groverlakshya.25.lg@gmail.com>
2025-08-02 00:40:11 +0530

    changed the cors for live deployment

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware to handle CORS
 const allowedOrigins = [];
 if (process.env.NODE_ENV == "production") {
-  allowedOrigins.push("https://connect-hq.vercel.app");
+  allowedOrigins.push("https://perfectmeraki-client.vercel.app");
 } else {
   allowedOrigins.push("http://localhost:3000");
   allowedOrigins.push("http://192.168.0.103:5173");
